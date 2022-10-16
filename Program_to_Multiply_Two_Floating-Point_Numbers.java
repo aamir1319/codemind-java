531f51dfd79f4406662cf4e85693d040aa2f65ed
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-16 08:09:50 +0530

    import java.util.*; class main{     public static void main(String args[]){     Scanner sc= new Scanner(System.in);     double a=sc.nextFloat();     double b=sc.nextFloat();     double mul=a*b;     System.out.format("%.2f",mul);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+    Scanner sc= new Scanner(System.in);
+    double a=sc.nextFloat();
+    double b=sc.nextFloat();
+    double mul=a*b;
+    System.out.format("%.2f",mul);
+    }
+}
