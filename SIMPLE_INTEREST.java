be482ca093789b751423fadf55217698194df2fa
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-16 12:07:56 +0530

    import java.util.*;
    class simple{
        public static void main(String args[]){
            Scanner sc = new Scanner(System.in);
            int p =sc.nextInt(),t=sc.nextInt(),r=sc.nextInt();
            int k;
            k=(p*t*r)/100;
            System.out.println(k);
        }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class simple{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int p =sc.nextInt(),t=sc.nextInt(),r=sc.nextInt();
+        int k;
+        k=(p*t*r)/100;
+        System.out.println(k);
+    }
+}
