56806320d03cf9469c008cb3dd7a480373ef8aa6
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-16 12:17:38 +0530

    import java.util.*;
    class converssion{
        public static void main(String args[]){
        Scanner sc = new Scanner(System.in);
        double n =sc.nextFloat();
        double a =((n*9)/5)+32;
        System.out.format("%.2f",a);
        }
    }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class converssion{
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    double n =sc.nextFloat();
+    double a =((n*9)/5)+32;
+    System.out.format("%.2f",a);
+    }
+}
