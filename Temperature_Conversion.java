68c828b1f8c08863a0b4f78588056a61bd6c78c0
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-16 07:55:57 +0530

    import java.util.*; class main{     public static void main (String args[]){         Scanner sc=new Scanner(System.in);         double a=sc.nextDouble();         double d=(a*9/5)+32;         System.out.format("%.2f",d);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class main{
+    public static void main (String args[]){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        double d=(a*9/5)+32;
+        System.out.format("%.2f",d);
+    }
+}
