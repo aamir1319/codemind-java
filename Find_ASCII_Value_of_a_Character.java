59863ecbe48b7f621912a9f947e8749a8f4abcf1
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-16 08:06:30 +0530

    import java.util.*; class main{     public static void main(String args[]){         Scanner sc =new Scanner(System.in);         char a=sc.next().charAt(0);         int ascii= a;         int castAscii=(int)a;         System.out.print(ascii);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        char a=sc.next().charAt(0);
+        int ascii= a;
+        int castAscii=(int)a;
+        System.out.print(ascii);
+    }
+}
