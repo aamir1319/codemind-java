d863efaf7ac3beea92b6f5a300a7928b718f436a
aamir1319 <108738138+aamir1319@users.noreply.github.com>
2022-10-16 12:30:03 +0530

    import java.util.*;
    class sol{
        
        static boolean prime(int n)
        {
            if(n==1 || n==0)
            {
                return false;
            }
            for(int i=2;i<n/2 + 1;i++)
            {
                if(n%i==0)
                {
                    return false;
                }
            }
            return true;
        }
        
        
        public static void main(String args[])
        {
            Scanner sc = new Scanner(System.in);
            int n = sc.nextInt();
            int [] arr = new int[n];
            for(int i=0;i<n;i++)
            {
                arr[i] = sc.nextInt();
            }
            int k = sc.nextInt(),c=0;
            for(int i=0;i<n;i++)
            {
                if (prime(arr[i])&&arr[i]<=k)
                {
                    c++;
                }
            }
            System.out.print(c);
        }
    }

---
+++ primes_less_than_k.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class sol{
+    
+    static boolean prime(int n)
+    {
+        if(n==1 || n==0)
+        {
+            return false;
+        }
+        for(int i=2;i<n/2 + 1;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt(),c=0;
+        for(int i=0;i<n;i++)
+        {
+            if (prime(arr[i])&&arr[i]<=k)
+            {
+                c++;
+            }
+        }
+        System.out.print(c);
+    }
+}
